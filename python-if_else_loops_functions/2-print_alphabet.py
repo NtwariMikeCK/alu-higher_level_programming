aa7c004433d8c9171d52c9997457374231809a97
NtwariMikeCK <k.ntwari@alustudent.com>
2024-06-23 05:30:44 -0700

    keep learning python

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print("".join("{:c}".format(c) for c in range(ord('a'), ord('z') + 1)), end="")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print("".join("{:c}".format(c) for c in range(ord('a'), ord('z') + 1) if c not in [ord('q'), ord('e')]), end="")
+
