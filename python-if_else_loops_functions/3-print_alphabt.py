188b8a3079cca6f2c264e93a5dd90132e5038b36
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-24 09:33:51 +0200

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(c) for c in range(ord('a'), ord('z') + 1) if c not in [ord('q'), ord('e')]), end="")
-
+print("".join(
+    "{}".format(chr(i)) for i in range(97, 123) if i not in (101, 113)
+), end="")
