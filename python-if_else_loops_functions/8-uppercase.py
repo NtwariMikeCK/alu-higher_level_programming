42b532bb4363bdbbb4fedb5c97c656c428a074cd
NtwariMikeCK <k.ntwari@alustudent.com>
2024-06-24 04:03:17 -0700

    hello

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
+
+
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
