8b500c20b62a86b476f745685ca7022b9eeb02c9
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-26 11:34:05 +0200

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import marshal
 import types
 
+
 def main():
     with open('hidden_4.pyc', 'rb') as f:
         f.seek(16)  # Skip the header of the .pyc file
@@ -12,6 +13,7 @@ def main():
     names = [name for name in dir(module) if not name.startswith('__')]
     for name in sorted(names):
         print(name)
+
 
 if __name__ == "__main__":
     main()
