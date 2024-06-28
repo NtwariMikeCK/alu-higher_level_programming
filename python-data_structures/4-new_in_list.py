7212b912c004f4493d52361af605034ca85b0a04
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-28 21:48:53 +0200

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,7 +4,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         # Return a copy of the original list
         return my_list[:]
-    
     # Create a copy of the original list
     new_list = my_list[:]
     # Replace the element at the specified index
