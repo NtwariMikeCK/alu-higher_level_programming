6bc566fe7851e0257ee443038d23e3a128486aac
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-28 21:45:38 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     sentence = "At school, I learnt C!"
     length, first = multiple_returns(sentence)
     print("Length: {:d} - First character: {}".format(length, first))
-
