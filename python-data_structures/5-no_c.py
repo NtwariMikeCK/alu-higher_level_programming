c4c25d02b3ce12c402fb5dbfae2251b08550f632
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-28 12:48:20 +0200

    Update 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -17,4 +17,3 @@ if __name__ == "__main__":
     print(no_c("Best School"))  # Output: Best Shool
     print(no_c("Chicago"))      # Output: hiago
     print(no_c("C is fun!"))    # Output:  is fun!
-
