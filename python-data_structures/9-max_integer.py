0d82bf73328080fa739c09c3941625fa61c46186
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-06-28 21:44:32 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythpn3
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
         return None
@@ -15,4 +15,3 @@ if __name__ == "__main__":
     my_list = [1, 90, 2, 13, 34, 5, -13, 3]
     max_value = max_integer(my_list)
     print("Max: {}".format(max_value))
-
