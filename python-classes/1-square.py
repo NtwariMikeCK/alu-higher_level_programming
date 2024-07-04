d4b9a01b1394cb97ee827d3547002672645ec294
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-04 12:26:28 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" This is a class that defines a square with it's side"""
+""" 
+This is a class that defines a square with it's side
+"""
 
 
-class Square:  """ This is the square class"""   
-
+class Square:     
+""" 
+This is the square class
+"""
     def __init__(self, size):
-        """ this is the side of the square"""
+        """ 
+        this is the side of the square
+        """
         self.size = size    
