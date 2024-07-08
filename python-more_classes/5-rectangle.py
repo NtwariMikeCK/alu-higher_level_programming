cd90dced536a4c56227dcc59481e6f2995d6fc49
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-08 11:04:39 +0200

    Update 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is a class that defines a rectangle and helps us to calculate the area and mean"""
+"""This is a class that defines a rectangle and helps us"""
 
 
 class Rectangle:
@@ -12,12 +12,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get the width of the rectangle."""
+        """Get the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
+        """Set the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
+        """Get the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
+        """Set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,17 +39,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Return the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
+        """Return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return the printable representation of the rectangle """
+        """Return the printable representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
@@ -59,5 +59,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """Print a message when an instance of Rectangle is """
+        """Print a message when an instance"""
         print("Bye rectangle...")
