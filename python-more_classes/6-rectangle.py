db836509b69548830d3753fafcd8bf08ccefb1a2
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-08 23:35:22 +0200

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -38,27 +38,28 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    
     def area(self):
-        """Return the area of the rectangle"""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle"""
+        """Return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return the printable representation of the rectangle"""
+        """Return the printable representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a string representation of the rectangle"""
+        """Return a string representation of the rectangle."""
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """Print a message when an instance"""
-        Rectangle.number_of_instamces -= 1
+        """Print a message when an instance of Rectangle is dele"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
