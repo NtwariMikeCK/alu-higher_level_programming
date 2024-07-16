b9ad925c64721741412c6b8c27a46f27dbb81053
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-16 12:34:06 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 
 
 """This is for creating a new python read function"""
-def read_file(filename=""):
+def read_file(filename):
     """start by defining the function"""
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
