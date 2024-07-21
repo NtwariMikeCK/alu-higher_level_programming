fff59018aded2476766beb8ff797897df9582e3f
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-21 19:39:51 +0200

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
--- Select tv_shows.title and tv_show_genres.genre_id, ordered by tv_shows.title and tv_show_genres.genre_id
+-- genre id by show
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
