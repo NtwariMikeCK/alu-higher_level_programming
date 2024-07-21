a1662073ba928f6498377c85c61141362695f788
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-21 10:37:03 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,5 @@
--- Create users
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
+-- List all privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- List all privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
