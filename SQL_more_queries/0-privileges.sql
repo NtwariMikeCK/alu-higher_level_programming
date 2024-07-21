60770e1d3605385b50e05f06834fdbf7df749b77
NtwariMikeCK <k.ntwari@alustudent.com>
2024-07-21 01:29:13 -0700

    my first work

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This query lists all the priveledges of user_0d_1
+-- This query lists all the priveledges of user_0d_2
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
