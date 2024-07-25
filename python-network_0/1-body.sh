e25435bcc44d7208f9dab2de9b0acb2a29075582
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-25 12:50:26 +0200

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request and display the body if the status code is 200
-curl -s -w "%{http_code}" -o /tmp/body.txt "$1" | grep -q '200' && cat /tmp/body.txt
+# Send GET request using curl and store response body in a variable
+curl -sL "$1"
