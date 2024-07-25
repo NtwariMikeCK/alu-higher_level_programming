a6cddd21fdc8bddeed4076520473af481eb8aac4
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-25 12:41:54 +0200

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Send a GET request to the URL and get both headers and body
-response=$(curl -s -w "%{http_code}" -o /tmp/body.txt "$1")
-
-# Check if the status code is 200
-if [ "$response" -eq 200 ]; then
-    # Display the body of the response
-    cat /tmp/body.txt
-fi
+# Send a GET request and display the body if status code is 200
+response=$(curl -s -w "%{http_code}" -o /tmp/body.txt "$1") && [ "$response" -eq 200 ] && cat /tmp/body.txt
