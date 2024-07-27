845fc7921c0ee3d035df2c3431db7c1937603b88
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-27 23:55:24 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-This script sends a request to a given URL and displays the value of the
-X-Request-Id variable found in the response headers.
-
-Usage:
-    ./5-hbtn_header.py <URL>
-
-Requirements:
-    - The first argument is the URL.
-    - Only requests and sys modules are allowed.
-"""
+"""Fetches https://alu-intranet.hbtn.io/status using requests."""
 
 import requests
 
-if __name__ == "__main__":
+def fetch_status():
+    """Fetches the content of a URL and prints its type and content."""
     url = 'https://alu-intranet.hbtn.io/status'
     response = requests.get(url)
     body = response.text
 
     print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+
+if __name__ == "__main__":
+    fetch_status()
