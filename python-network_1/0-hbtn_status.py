09b7066f2ac7894356db1135cf6c144f025f2e4b
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-27 23:39:22 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-This script takes in a URL, sends a request to the URL, and displays the.
-the X-Request-Id variable found in the header of the response.
-"""
+"""Documented"""
 import urllib.request
-#imports the requests.
-def fetch_status():
-#Fetches the content of a URL and prints its type, cont.
-    url = 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-#opens the url.
+
+url = 'https://alu-intranet.hbtn.io/status'
+
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    content = response.read()
     print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {body.decode('utf-8')}")
-#the print .
-if __name__ == "__main__":
-    fetch_status()
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode("utf-8"))
