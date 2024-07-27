5035cb0a58f160dc7428c0b1cb78f6d88ca76f9d
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-27 23:46:30 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""A script to fetch and display the content from a specific URL."""
+"""
+A script to fetch and display the content from a specific URL.
+"""
 
-import urllib.request  # Importing the urllib.request module to handle URL requests
+import urllib.request
 
 def fetch_status():
-    """Fetches the content of a URL and prints its type, content, and UTF-8 decoded content."""
-    
-    url = 'https://alu-intranet.hbtn.io/status'  # The URL to be fetched
+    """Fetches content from a URL and prints its details."""
+    url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        # Opens the URL and retrieves the response
         body = response.read()
-        # Reads the content of the response
         
-    # Prints the details of the response body
     print("Body response:")
-    print(f"    - type: {type(body)}")  # Prints the type of the response body
-    print(f"    - content: {body}")  # Prints the raw content of the response body
-    print(f"    - utf8 content: {body.decode('utf-8')}")  # Prints the UTF-8 decoded content of the response body
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
 
-# If this script is executed directly, the fetch_status function is called
 if __name__ == "__main__":
     fetch_status()
