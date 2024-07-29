b0ee30303f6e480a633ed23ef19592495b32eb12
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-29 13:47:39 +0200

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,7 +14,6 @@ class State(Base):
     State class to map to the 'states' table in MySQL.
     """
     __tablename__ = 'states'
-    
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
@@ -23,6 +22,5 @@ if __name__ == "__main__":
     from sys import argv
     # Create an engine that connects to the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    
     # Create all tables in the database (in this case, just 'states')
     Base.metadata.create_all(engine)
