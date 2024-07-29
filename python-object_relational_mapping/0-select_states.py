837ed91b74c6887e3049dfe192f0547657240d23
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-29 11:15:31 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""mysql work is docummented here"""
+"""
+mysql work is docummented here
+"""
 
 import MySQLdb
 import sys
@@ -15,16 +17,12 @@ db = database
 def list_states(username, password, database):
     # Connect to the MySQL server
     db = MySQLdb.connect(host, port, user, passwd, db)
-    
     # Create a cursor object
     cursor = db.cursor()
-    
     # Execute the query to get all states sorted by id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
     # Fetch all rows from the executed query
     states = cursor.fetchall()
-    
     # Print the results in the specified format
     for state in states:
         print(state)
