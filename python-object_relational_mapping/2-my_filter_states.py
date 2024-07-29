b6f2b718b1a97e7982577909979206b0e356eddf
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-29 13:08:29 +0200

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""mysql"""
+
 import MySQLdb
 import sys
 
 def filter_states(username, password, database, state_name):
     """
-    Connects to a MySQL database and lists all states with names matching the given state_name,
+    Connects to a MySQL database and lists all statesthe given state_name,
     sorted by id.
 
     Args:
@@ -21,29 +23,25 @@ def filter_states(username, password, database, state_name):
         passwd=password,
         db=database
     )
-    
     # Create a cursor object
     cursor = db.cursor()
-    
-    # Execute the query to get states with names matching the provided state_name
+    # Execute the query to get states with ovided state_name
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (state_name,))
-    
     # Fetch all rows from the executed query
     states = cursor.fetchall()
-    
     # Print the results in the specified format
     for state in states:
         print(state)
-    
     # Close the cursor and database connection
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     """
-    Entry point of the script. Expects exactly 4 command-line arguments:
-    mysql username, mysql password, database name, and state name. If the number of
+    Entry point of the script. Expects exactly 4  arguments:
+    mysql username, mysql password, database name,. If the number of
     arguments is incorrect, prints a usage message.
     """
     # Check if there are exactly 4 arguments (script name + 4 arguments)
