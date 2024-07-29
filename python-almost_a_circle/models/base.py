ae20c7b1b0a697a3d01d2e2f9a2a39afe3cf4986
NtwariMikeCK <k.ntwari@alustudent.com>
2024-07-29 01:04:25 -0700

    start working on the project

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# python-almost_a_circle/models/base.py
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
