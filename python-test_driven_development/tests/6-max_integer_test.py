99579783a1403c7393f8f23df3a9ce6b7ca90187
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-07-31 22:44:05 +0200

    Update 6-max_integer_test.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+""""max integer"""
+
+
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    """max integer test"""
 
-    def test_max_in_middle(self):
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    def test_simple_case(self):
+        """test simple cases"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
-    def test_one_negative_number(self):
-        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+    def test_negative_case(self):
+        """test negative cases"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
-    def test_empty_list(self):
+    def test_empty_case(self):
+        """test empty cases"""
         self.assertEqual(max_integer([]), None)
 
-    def test_one_element(self):
+    def test_max_middle_case(self):
+        """test max middle"""
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+
+    def test_one_element_case(self):
+        """tests one element"""
         self.assertEqual(max_integer([5]), 5)
-
-    def test_floats(self):
-        self.assertEqual(max_integer([1.1, 2.2, 3.3, 4.4]), 4.4)
-
-    def test_mixed_floats_and_ints(self):
-        self.assertEqual(max_integer([1, 2.2, 3, 4.4]), 4.4)
-
-    def test_all_same_value(self):
-        self.assertEqual(max_integer([7, 7, 7, 7]), 7)
-
-    def test_none(self):
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def test_non_int_elements(self):
-        with self.assertRaises(TypeError):
-            max_integer([1, "two", 3, 4])
-
-if __name__ == '__main__':
-    unittest.main()
