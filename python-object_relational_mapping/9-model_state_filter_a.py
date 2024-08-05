5a8586076202f70cf38015687aa33ec06f7670db
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-08-05 09:36:47 +0200

    Update 9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
 """
-This script lists all State objects that contain the letter 'a'
-from the database hbtn_0e_6_usa.
+List all State objects that contain the letter a from a database
 """
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
-import sys
 
 if __name__ == "__main__":
-    # Ensure there are exactly 3 arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./9-model_state_filter_a.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
-    
-    # Extract arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    # Create an engine that connects to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
-    
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    
-    # Create a Session instance
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    
-    # Query for State objects that contain the letter 'a' in the name
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    
-    # Print the results
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
     for state in states:
-        print(f"{state.id}: {state.name}")
-    
-    # Close the session
+        print("{}: {}".format(state.id, state.name))
     session.close()
