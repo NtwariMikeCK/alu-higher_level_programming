9f644e53690eaba161e8c49a80480b8b316dc0c0
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-08-05 09:38:42 +0200

    Update 11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
 """
-This script adds a new State object “Louisiana” to the database hbtn_0e_6_usa
-and prints the new state's id.
+Adds the State object Louisiana to a database
 """
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
-import sys
+
 
 if __name__ == "__main__":
-    # Ensure there are exactly 3 arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./11-model_state_insert.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
-    
-    # Extract arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    # Create an engine that connects to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
-    
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    
-    # Create a Session instance
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    
-    # Create a new State object
-    new_state = State(name="Louisiana")
-    
-    # Add the new State object to the session
+    new_state = State(name='Louisiana')
     session.add(new_state)
-    
-    # Commit the transaction to the database
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(str(state.id))
     session.commit()
-    
-    # Print the id of the newly created State object
-    print(new_state.id)
-    
-    # Close the session
     session.close()
