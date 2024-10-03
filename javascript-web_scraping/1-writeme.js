e1d80344ed6a1102c275c7164ee12d4b41a7a25c
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-10-03 09:55:15 +0200

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Check if the file path and string to write are provided as arguments
+// Get the file path and the string to write from the arguments
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 if (!filePath || !stringToWrite) {
-  console.error('Usage: ./1-writeme.js <file path> "<string to write>"');
+  console.error('Usage: ./1-writeme.js <file path> <string to write>');
   process.exit(1);
 }
 
-// Write the string to the file in utf-8
+// Write the string to the file in UTF-8 encoding
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
-    console.error(err); // Print the error object if an error occurs
+    console.error(err); // Print the error if writing to the file fails
   }
 });
